8eacce94115e6f2f11ef534c26143c132c6c3eb7
xcheche <chekwubechukwu23@gmail.com>
2024-06-12 17:34:32 +0100

    done django normal form

--- ep13/app1/forms.py
+++ ep13/app1/forms.py
@@ -5,6 +5,11 @@ from django import forms
 
 class StudentInfo(forms.Form):
     name = forms.CharField(
+        label="First Name",
+        label_suffix="-",
+        min_length=2,
+        max_length=15,
+        initial="Name",
         required=True,
         widget=forms.TextInput(
             attrs={
@@ -15,16 +20,20 @@ class StudentInfo(forms.Form):
         ),
     )
     email = forms.EmailField(
+        label="Email",
+        initial="Email",
         required=True,
         widget=forms.EmailInput(
             attrs={
                 "placeholder": "Enter your email",
                 "required": "required",
                 "class": "form-control",
-            }
+            },
         ),
     )
     message = forms.CharField(
+        label="Your Message",
+        initial="Message",
         required=True,
         widget=forms.Textarea(
             attrs={
@@ -35,10 +44,12 @@ class StudentInfo(forms.Form):
         ),
     )
     phone_no = forms.IntegerField(
+        label="Phone",
+        initial="Phone",
         required=True,
         widget=forms.NumberInput(
             attrs={
-                "placeholder": "Enter your phone number",
+                "placeholder": "Enter your phone number pls",
                 "required": "required",
                 "class": "form-control",
             }
--- ep13/app1/views.py
+++ ep13/app1/views.py
@@ -12,3 +12,21 @@ def home(request):
         fm = StudentInfo()
         print(f"get data {fm}")
     return render(request, "home.html", {"form": fm})
+
+
+# formfields directly in views.py
+# def home(request):
+#     if request.method == "POST":
+#         fm = StudentInfo(request.POST)
+#         print(f"post data {fm}")
+#     else:
+#         fm = StudentInfo(  # from here
+#             label_suffix="-",
+#             initial={
+#                 "name": "Full Name",
+#                 "email": "Email",
+#                 "phone": "Phone",
+#             },
+#         )
+#         print(f"get data {fm}")
+#     return render(request, "home.html", {"form": fm})
